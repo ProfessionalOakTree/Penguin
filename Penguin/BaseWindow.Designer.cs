08db7bb905141e4361715f9602145b655762ae42
Joshua Busacker <joshua@busacker.xyz>
2020-08-11 12:40:19 +1000

    Setup window attributes.

---
+++ Penguin/BaseWindow.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Penguin
+{
+    partial class BaseWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Location = new System.Drawing.Point(12, 12);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(256, 256);
+            this.PictureBox.TabIndex = 0;
+            this.PictureBox.TabStop = false;
+            // 
+            // BaseWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 280);
+            this.Controls.Add(this.PictureBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BaseWindow";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox;
+    }
+}
+
--- Penguin/BaseWindow.cs
+++ Penguin/BaseWindow.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Penguin
 {
-    public partial class Form1 : Form
+    public partial class BaseWindow : Form
     {
-        public Form1()
+        public BaseWindow()
         {
             InitializeComponent();
         }
